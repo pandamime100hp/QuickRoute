b0ec1a969aff0c7805429251f059d2cf3d7d8912
Makar Emeliyanov <66727639+Pandamime100hp@users.noreply.github.com>
2025-03-10 20:42:42 +0100

    feat(providers): add `tomtom` provider
    - Added `src/providers/tomtom.ts`
    - Added tests to validate new `tomtom` provider

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1" // Maps "@/file" to "src/file"
+  }
 };
 
 export default config;
---
+++ src/providers/tomtom.ts
@@ -0,0 +1,42 @@
+import { ProviderInterface } from "@/provider-interface";
+
+// TomTom API docs: https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
+
+
+class TomTomProvider implements ProviderInterface {
+    apiKey?: string;
+    baseUrl: string;
+    apiVersion: string;
+    extension: string;
+    countrySet: string;
+
+    constructor(apiKey?: string) {
+        this.apiKey = process.env.TOMTOM_API_KEY || apiKey;
+        this.baseUrl = process.env.TOMTOM_BASE_URL || "api.tomtom.com";
+        this.apiVersion = process.env.TOMTOM_API_VERSION || "2";
+        this.extension = process.env.TOMTOM_EXTENSION || "json";
+        this.countrySet = this.setCountrySet();
+
+        if (!this.apiKey) {
+            throw new Error("TomTom API Key Not Found. Please set `TOMTOM_API_KEY` environment variable.");
+        }
+    }
+
+    async search(query: string): Promise<string> {
+        let encodedQuery: string = encodeURIComponent(query);
+        let endpoint: string = this.generateEndpoint(encodedQuery);
+
+        const results: Response = await fetch(endpoint);
+        return String(await results.json());
+    }
+
+    generateEndpoint(query: string): string {
+        return `https://${this.baseUrl}/search/${this.apiVersion}/search/${query}.${this.extension}?key=${this.apiKey}&countrySet=${this.countrySet}`;
+    }
+
+    setCountrySet(): string {
+        return (process.env.TOMTOM_COUNTRY_SET?.split(" ") || ["AU"]).join(",");
+    }
+}
+
+export default TomTomProvider;
